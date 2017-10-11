84f2b37821f25edf466d3a2cdd2c89f0683f30e0
Nacho <ignacio.navarro14@imperial.ac.uk>
2017-10-11 13:41:15 +0100

    Allow user to select different conventional hashing functions

---
+++ balloon.py
@@ -0,0 +1,12 @@
+import hashlib
+import random
+import string
+
+hash_functions = {
+    'md5': hashlib.md5,
+    'sha1': hashlib.sha1,
+    'sha224': hashlib.sha224,
+    'sha256': hashlib.sha256,
+    'sha384': hashlib.sha384,
+    'sha512': hashlib.sha512
+}
